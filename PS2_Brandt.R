488cbd74afbbaa1a595e2c0dba4cdfd3653342c9
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-05 12:09:51 -0600

    Full Cho-gains Calculation
    Lines 19:31 calculate the Cho-gains d statistic.

--- PS2_Brandt.R
+++ PS2_Brandt.R
@@ -14,3 +14,19 @@ percent_votes <- integer_votes/total_votes
 logarithm <- log(1+1/ones_digits, base = 10)
 leemis_set <- percent_votes - logarithm
 leemis_m <- max(leemis_set)
+
+
+# Cho-gains d
+ones_digits <- c(1:9)
+integer_votes <- NULL
+for (i in ones_digits){
+  integer_votes <- c(integer_votes, length(test_data[as.numeric(substr(test_data,1,1)) == i]))
+}
+total_votes <- length(test_data)
+percent_votes <- integer_votes/total_votes
+logarithm <- log(1+1/ones_digits, base = 10)
+cho_set <- percent_votes - logarithm
+cho_set_sq <- cho_set^2
+sum_cho <- sum(cho_set_sq)
+cho_d <- sqrt(sum_cho)
+
