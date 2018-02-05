e89bb0574e2e19fee8b04607773344160c5f4843
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-05 12:11:40 -0600

    Calculate both at once
    Lines 33:46 will calculate both the leemis m and the cho-gains d statistics at the same time in fewer steps than running the two calculations separately.

--- PS2_Brandt.R
+++ PS2_Brandt.R
@@ -30,3 +30,18 @@ cho_set_sq <- cho_set^2
 sum_cho <- sum(cho_set_sq)
 cho_d <- sqrt(sum_cho)
 
+# Combine the two
+ones_digits <- c(1:9)
+integer_votes <- NULL
+for (i in ones_digits){
+  integer_votes <- c(integer_votes, length(test_data[as.numeric(substr(test_data,1,1)) == i]))
+}
+total_votes <- length(test_data)
+percent_votes <- integer_votes/total_votes
+logarithm <- log(1+1/ones_digits, base = 10)
+leemis_set <- percent_votes - logarithm
+leemis_m <- max(leemis_set)
+cho_set_sq <- leemis_set^2
+sum_cho <- sum(cho_set_sq)
+cho_d <- sqrt(sum_cho)
+
