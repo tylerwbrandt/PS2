f65a2c67d828965ab076b6ad9e9804e1ba965a1d
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-05 11:57:49 -0600

    Function for finding frequency of first digit
    Lines 7-11 show how to get the number of times that each number from 1:9 appears as the first digit in my data.

--- PS2_Brandt.R
+++ PS2_Brandt.R
@@ -1,1 +1,12 @@
 ## Tyler Brandt - Problem Set 2
+
+rm(list = ls())
+test_data <- c(1:100)
+
+# Leemis M
+ones_digits <- c(1:9)
+integer_votes <- NULL
+for (i in ones_digits){
+  integer_votes <- c(integer_votes, length(test_data[as.numeric(substr(test_data,1,1)) == i]))
+}
+
